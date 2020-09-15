bf89cdec6624d72d97e8338575f0259ee794d8e7
emstyle <59380416+ephicks@users.noreply.github.com>
2020-09-14 19:56:46 -0700

    hotjar

--- js/style.js
+++ js/style.js
@@ -16,3 +16,23 @@ function scrollNav() {
     });
   }
   scrollNav();
+
+  function scrollBanner() {
+    $(document).on('scroll', function(){
+    var scrollPos = $(this).scrollTop();
+      $('.parallax-fade-top').css({
+        'top' : (scrollPos/2)+'px',
+        'opacity' : 1-(scrollPos/700)
+      });
+      $('.parallax-00').css({
+        'top' : (scrollPos/-3.5)+'px'
+      });
+      $('.parallax-01').css({
+        'top' : (scrollPos/-2.8)+'px'
+      });
+      $('.parallax-top-shadow').css({
+        'top' : (scrollPos/-2)+'px'
+      });
+    });    
+    }
+  scrollBanner();	  
