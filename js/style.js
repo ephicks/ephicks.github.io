37dad4dc0eb622eeafcb2be5119d1c3fe2304afa
emstyle <59380416+ephicks@users.noreply.github.com>
2020-11-12 17:09:14 -0800

    updates
    many updated

--- js/style.js
+++ js/style.js
@@ -1,7 +1,7 @@
 function fade() {
     $('.preloader').fadeOut("slow");
     }
-    setTimeout(fade, 3000);
+    setTimeout(fade, 1000);
     
 
 function scrollNav() {
@@ -36,3 +36,19 @@ function scrollNav() {
     return false;
   });
   
+  $(function() {  
+    $('.btn-6')
+      .on('mouseenter', function(e) {
+        var parentOffset = $(this).offset(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top:relY, left:relX})
+      })
+      .on('mouseout', function(e) {
+        var parentOffset = $(this).offset(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top:relY, left:relX})
+      });
+    $('[href=#]').click(function(){return false});
+  });
